9594d02856e1fa174ab51326953b714e740116da
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-05-20 12:38:54 +0100

    Working map with weather layer form OpenWeatherMap

--- js/main.js
+++ js/main.js
@@ -186,66 +186,183 @@
 
         render: function(){
 
-            $.ajax({
-                url: "http://api.openweathermap.org/data/2.5/weather?q=London,uk",
-                method: "GET",
-                //async: false,
-                dataType: 'jsonp'
-            }).done(function(data){
-                console.log(data);
+            //$.ajax({
+            //    url: "http://api.openweathermap.org/data/2.5/weather?q=London,uk",
+            //    method: "GET",
+            //    //async: false,
+            //    dataType: 'jsonp'
+            //}).done(function(data){
+            //    console.log(data);
+            //    drawMap(data);
+            //});
 
-                //drawMap(data);
-            });
+            //function drawMap(data){
+                //var myLatlng = new google.maps.LatLng(data.coord.lat, data.coord.lon);
+                //var mapOptions = {
+                //    center: myLatlng,
+                //    zoom: 5,
+                //    styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#000000"},{"lightness":13}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#08304b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},{"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},{"featureType":"transit","elementType":"all","stylers":[{"color":"#146474"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#021019"}]}]    };
+                //var map = new google.maps.Map(document.getElementById('map'),
+                //    mapOptions);
+                //
+                //var contentString = '<div id="content">'+
+                //    '<div id="siteNotice">'+
+                //    '</div>'+
+                //    '<h1 id="firstHeading" class="firstHeading">Home</h1>'+
+                //    '<div id="bodyContent">'+
+                //    '<p><b>London, UK</b><br />'+
+                //    'God save the queen!'+
+                //    '</p>'+
+                //    '</div>'+
+                //    '</div>';
+                //
+                //var infowindow = new google.maps.InfoWindow({
+                //    content: contentString
+                //});
+                //
+                //var marker_london = new google.maps.Marker({
+                //    position: myLatlng,
+                //    map: map,
+                //    title: 'Where I live now'
+                //});
+                //google.maps.event.addListener(marker_london, 'click', function() {
+                //    infowindow.open(map,marker_london);
+                //});
 
-            function drawMap(data){
-                var myLatlng = new google.maps.LatLng(51.5072, 0.1275);
-                var mapOptions = {
-                    center: myLatlng,
-                    zoom: 3};
-                    //styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#000000"},{"lightness":13}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#08304b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},{"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},{"featureType":"transit","elementType":"all","stylers":[{"color":"#146474"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#021019"}]}]    };
-                var map = new google.maps.Map(document.getElementById('map'),
-                    mapOptions);
+                var map;
+                var geoJSON;
+                var request;
+                var gettingData = false;
+                var openWeatherMapKey = "3bec42d12c011f0949a7f42fdd71f77c";
+                function initialize() {
+                    var mapOptions = {
+                        zoom: 5,
+                        center: new google.maps.LatLng(51.5072,-0.1275)
+                    };
+                    map = new google.maps.Map(document.getElementById('map'),
+                        mapOptions);
+                    // Add interaction listeners to make weather requests
+                    google.maps.event.addListener(map, 'idle', checkIfDataRequested);
+                    // Sets up and populates the info window with details
+                    map.data.addListener('click', function(event) {
+                        infowindow.setContent(
+                            "<img src=" + event.feature.getProperty("icon") + ">"
+                            + "<br /><strong>" + event.feature.getProperty("city") + "</strong>"
+                            + "<br />" + event.feature.getProperty("temperature") + "&deg;C"
+                            + "<br />" + event.feature.getProperty("weather")
+                        );
+                        infowindow.setOptions({
+                            position:{
+                                lat: event.latLng.lat(),
+                                lng: event.latLng.lng()
+                            },
+                            pixelOffset: {
+                                width: 0,
+                                height: -15
+                            }
+                        });
+                        infowindow.open(map);
+                    });
+                };
+                var checkIfDataRequested = function() {
+                    // Stop extra requests being sent
+                    while (gettingData === true) {
+                        request.abort();
+                        gettingData = false;
+                    }
+                    getCoords();
+                };
+                // Get the coordinates from the Map bounds
+                var getCoords = function() {
+                    var bounds = map.getBounds();
+                    var NE = bounds.getNorthEast();
+                    var SW = bounds.getSouthWest();
+                    getWeather(NE.lat(), NE.lng(), SW.lat(), SW.lng());
+                };
+                // Make the weather request
+                var getWeather = function(northLat, eastLng, southLat, westLng) {
+                    gettingData = true;
+                    var requestString = "http://api.openweathermap.org/data/2.5/box/city?bbox="
+                        + westLng + "," + northLat + "," //left top
+                        + eastLng + "," + southLat + "," //right bottom
+                        + map.getZoom()
+                        + "&cluster=yes&format=json"
+                        + "&APPID=" + openWeatherMapKey;
+                    request = new XMLHttpRequest();
+                    request.onload = proccessResults;
+                    request.open("get", requestString, true);
+                    request.send();
+                };
+                // Take the JSON results and proccess them
+                var proccessResults = function() {
+                    console.log(this);
+                    var results = JSON.parse(this.responseText);
+                    if (results.list.length > 0) {
+                        resetData();
+                        for (var i = 0; i < results.list.length; i++) {
+                            geoJSON.features.push(jsonToGeoJson(results.list[i]));
+                        }
+                        drawIcons(geoJSON);
+                    }
+                };
+                var infowindow = new google.maps.InfoWindow();
+                // For each result that comes back, convert the data to geoJSON
+                var jsonToGeoJson = function (weatherItem) {
+                    var feature = {
+                        type: "Feature",
+                        properties: {
+                            city: weatherItem.name,
+                            weather: weatherItem.weather[0].main,
+                            temperature: weatherItem.main.temp,
+                            min: weatherItem.main.temp_min,
+                            max: weatherItem.main.temp_max,
+                            humidity: weatherItem.main.humidity,
+                            pressure: weatherItem.main.pressure,
+                            windSpeed: weatherItem.wind.speed,
+                            windDegrees: weatherItem.wind.deg,
+                            windGust: weatherItem.wind.gust,
+                            icon: "http://openweathermap.org/img/w/"
+                            + weatherItem.weather[0].icon  + ".png",
+                            coordinates: [weatherItem.coord.lon, weatherItem.coord.lat]
+                        },
+                        geometry: {
+                            type: "Point",
+                            coordinates: [weatherItem.coord.lon, weatherItem.coord.lat]
+                        }
+                    };
+                    // Set the custom marker icon
+                    map.data.setStyle(function(feature) {
+                        return {
+                            icon: {
+                                url: feature.getProperty('icon'),
+                                anchor: new google.maps.Point(25, 25)
+                            }
+                        };
+                    });
+                    // returns object
+                    return feature;
+                };
+// Add the markers to the map
+                var drawIcons = function (weather) {
+                    map.data.addGeoJson(geoJSON);
+                    // Set the flag to finished
+                    gettingData = false;
+                };
+// Clear data layer and geoJSON
+                var resetData = function () {
+                    geoJSON = {
+                        type: "FeatureCollection",
+                        features: []
+                    };
+                    map.data.forEach(function(feature) {
+                        map.data.remove(feature);
+                    });
+                };
 
-                var contentString = '<div id="content">'+
-                    '<div id="siteNotice">'+
-                    '</div>'+
-                    '<h1 id="firstHeading" class="firstHeading">Home</h1>'+
-                    '<div id="bodyContent">'+
-                    '<p><b>London, UK</b><br />'+
-                    'God save the queen!'+
-                    '</p>'+
-                    '</div>'+
-                    '</div>';
-
-                var infowindow = new google.maps.InfoWindow({
-                    content: contentString
-                });
-
-                var marker_london = new google.maps.Marker({
-                    position: myLatlng,
-                    map: map,
-                    title: 'Where I live now'
-                });
-                google.maps.event.addListener(marker_london, 'click', function() {
-                    infowindow.open(map,marker_london);
-                });
+                initialize();
             }
-
-            //this.serviceResponse = hub.serviceResponse;
-            //var coords = serviceResponce.coord;
-
-            //console.log(hub.serviceResponse);
-
-            //var mapLocation = new google.maps.LatLng(serviceResponce.coord.lat, serviceResponce.coord.lon);
-            //
-            //var mapOptions = {
-            //    center: mapLocation,
-            //    zoom: 3,
-            //    styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#000000"},{"lightness":13}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#08304b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},{"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},{"featureType":"transit","elementType":"all","stylers":[{"color":"#146474"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#021019"}]}]    };
-            //var map = new google.maps.Map(this.map,
-            //    mapOptions);
-        }
-    };
+        };
+    //};
 
     hub.init();
 
